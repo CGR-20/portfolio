314cfdff836c69e19eb36dfd18dfff227aba3cca
Carlos Ramirez <carramirez2610@gmail.com>
2026-01-12 23:10:45 -0700

    made a bunch of changes. i don't really like all of them so i might get rid of the changes and start again.

--- scripts/root_header_footer_injection.js
+++ scripts/root_header_footer_injection.js
@@ -1,22 +1,37 @@
 // Inject partials into header/footer
 // this will allow me to avoid repeating code in multiple files
 function loadPartial(id, file) {
+  // inject header/footer html
   fetch(file)
     .then(res => res.text())
     .then(data => {
       document.getElementById(id).innerHTML = data;
 
-      // inject header specific code
+      // modify header specific code
       if (id === "header") {
-        const current = location.pathname.split("/").pop().replace(".html", "") || "index";
+        // work on other stuff instead
+        /*
+        const container = document.querySelector("header");
 
-        // highlight the current page in the nav
+        const allElements = container.querySelectorAll("nav a");
+
+        allElements.forEach(element => {
+          console.log(element.text)
+        });
+        */
+
+        /*
+        //const current = location.pathname.split("/").pop().replace(".html", "") || "index";
+
+        // highlight the current page in the nav (currently now working)
+        //document.querySelectorAll(`#${id} nav a`).forEach(link => {
         document.querySelectorAll(`#${id} nav a`).forEach(link => {
           if (link.dataset.page === current) link.classList.add("active");
         });
+        */
       }
 
-      // inject footer specific code
+      // modify footer specific code
       if (id === "footer") {
         // update the year in the footer automatically
         const yearEl = document.getElementById("year");
